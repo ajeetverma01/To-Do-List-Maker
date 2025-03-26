642ac8370b9780e53717147974c26e44b6b8ec42
Ajeet Verma <av401402@gmail.com>
2025-03-26 09:27:15 +0530

    Fuction completed

--- script.js
+++ script.js
@@ -2,4 +2,27 @@ const inputBox = document.getElementById("input-box");
 const listCont = document.getElementById("list-cont");
 
 function AddTask() {
+    if (inputBox.value.trim() === '') {
+        Swal.fire({
+            title: "Oops!",
+            text: "Enter a task before adding!",
+            icon: "warning",
+            background: "#1e1e1e",
+            color: "#fff",
+            confirmButtonColor: "#ff477e"
+        });
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+
+        li.style.opacity = "0";
+        listCont.appendChild(li);
+        setTimeout(() => li.style.opacity = "1", 100);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
 }
