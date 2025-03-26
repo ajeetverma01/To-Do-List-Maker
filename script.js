952d90399f235be0290a2f127774771767c4aabb
Ajeet Verma <av401402@gmail.com>
2025-03-26 09:29:44 +0530

    Js cmpleted

--- script.js
+++ script.js
@@ -20,9 +20,45 @@ function AddTask() {
         setTimeout(() => li.style.opacity = "1", 100);
 
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        span.innerHTML = "\u00d7"; // Unicode for × (close)
         li.appendChild(span);
     }
     inputBox.value = "";
     saveData();
 }
+
+listCont.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This task will be deleted!",
+            icon: "warning",
+            showCancelButton: true,
+            background: "#1e1e1e",
+            color: "#fff",
+            confirmButtonColor: "#ff477e",
+            cancelButtonColor: "#555",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                e.target.parentElement.style.opacity = "0";
+                setTimeout(() => {
+                    e.target.parentElement.remove();
+                    saveData();
+                }, 300);
+            }
+        });
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listCont.innerHTML);
+}
+
+function showList() {
+    listCont.innerHTML = localStorage.getItem("data");
+}
+showList();
